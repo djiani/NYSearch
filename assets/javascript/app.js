20529843928ad0e386e5681573e5e793310a8b63
Uncle Sam <djiasara@gmail.com>
2019-04-26 17:11:59 -0400

    retrieve value form NYT api and displaying them"
    exit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,53 +1,75 @@
-function getNews(title, link, imgURL) {
-  return `
-    <div>
-        <h2 class="title">${title}</h2>
-        <img src =${imgURL} />
-        <a href=${link}>link </a>
+function getNews(title, link, imgURL, snippet) {
+    return `
+    <div class="card m-2 d-inline-flex" style="width:80% heigh:300px">
+            <div class="card-body">
+                <h4 class="card-title">${title}</h4>
+                <p class="card-text">${snippet}</p>
+                <a href=${link} class="btn btn-primary">More...</a>
+            </div>
+            <img class="card-img-botton" src=${imgURL} alt=${imgURL} style="width:100%">
     </div>`;
-}
-const baseUrl = "https://api.nytimes.com/svc/search/v2/";
-
+    
+        }
+        const baseUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=9GJooULQWNQ78BGGiBYrgraZtLeqaEP9";
+        const baseIMG = "https://www.nytimes.com/";
+        
+                        
+        
 $(document).ready(function() {
-  $("#submitSearch").click(function(event) {
-    event.preventDefault();
+            $("#submitSearch").click(function (event) {
+                event.preventDefault();
+                let querystring = "";
+                let searchTerm = $("#searchID").val();
+                console.log(searchTerm);
 
-    let searchTerm = $("#searchID").val();
-    console.log(searchTerm);
+                let numberRecords = $("#numRecord").val();
+                console.log("numberRecords");
+                console.log(numberRecords);
 
-    let numberRecords = $("#numRecord").val();
-    console.log("numberRecords");
-    console.log(numberRecords);
+                let startingYear = $("#startYearID").val();
+                console.log(startingYear);
 
-    let startingYear = $("#startYearID").val();
-    console.log(startingYear);
+                let endYear = $("#endYearID").val();
+                console.log(endYear);
+                querystring += `&q=${searchTerm}&page=${numberRecords}`;
+                if (startingYear) {
+                    querystring += "begin_date=&" + startingYear + "0101";
 
-    let endYear = $("#endYearID").val();
-    console.log(endYear); 
-    //ajax code function here:
-    $.ajax({
-      url:`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchTerm}&api-key=9GJooULQWNQ78BGGiBYrgraZtLeqaEP9`,
-      method: "GET"
+                }
+                if (endYear) {
+                    querystring += "end_date=&" + endYear + "0101";
 
-    }).then(function(response){
-      const articles = response.response.docs.map(function(newArt){
-        return getNews(newArt.snippet, "", "");
-      })
+                }
+                //ajax code function here:
+                $.ajax({
+                    url: baseUrl + querystring,
+                    method: "GET"
 
-      $("#searchResult").append(articles);
+                }).then(function (response) {
+                    console.log(response);
+                    const articles = response.response.docs.map(function (newArt) {
+                        let imgurl = "";
+                        if (newArt.multimedia && newArt.multimedia[0]) {
+                            imgurl = newArt.multimedia[0].url
+                        }
+                        console.log(baseIMG+imgurl);
+                        return getNews(newArt.headline.main, newArt.web_url, baseIMG + imgurl, newArt.snippet);
+                    })
 
-    }).catch(function(err){
-      console.log("error");
-    })
+                    $("#searchResult").append(articles);
+
+                }).catch(function (err) {
+                    console.log("error");
+                })
 
 
 
 
 
 
-    // $("#searchResult").append(getNews("news1", "link", "url1"));
-    // $("#searchResult").append(getNews("news2", "link", "url3"));
+                // $("#searchResult").append(getNews("news1", "link", "url1"));
+                // $("#searchResult").append(getNews("news2", "link", "url3"));
 
-  });
+            });
 
-});
+        });
